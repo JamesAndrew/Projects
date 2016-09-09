9400ddb21a692b9971227d4fecd823d293f34dda
David Rice <davidrice363@gmail.com>
2016-09-08 20:27:51 -0600

    Added GreedySolver class. It isn't needed but we can use it as a way to get a sense of how this project will work

---
+++ CSCI446_Project1_GraphColoring/src/GraphColoring/GreedySolver.java
@@ -0,0 +1,14 @@
+package GraphColoring;
+
+/**
+ * This strategy is not needed but is used as an initial approach to get a sense
+ * of how this program will work
+ * @version 09/08/16
+ */
+public class GreedySolver extends ConstraintSolver
+{
+    public GreedySolver()
+    {
+        
+    }
+}
