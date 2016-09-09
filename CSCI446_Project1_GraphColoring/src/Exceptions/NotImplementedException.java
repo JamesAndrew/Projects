b4cc494c6534f3aece42ce4bafb1da5c227e0a22
David Rice <davidrice363@gmail.com>
2016-09-08 20:03:12 -0600

    Added abstract ConstraintSolver class, SatisfiesConstraint() method, and Exceptions package with NotImplementedException

---
+++ CSCI446_Project1_GraphColoring/src/Exceptions/NotImplementedException.java
@@ -0,0 +1,10 @@
+package Exceptions;
+
+/**
+ * Custom exception class
+ * @version 09/08/16
+ */
+public class NotImplementedException extends RuntimeException
+{
+    public NotImplementedException() { }
+}
---
+++ CSCI446_Project1_GraphColoring/src/GraphColoring/ConstraintSolver.java
@@ -0,0 +1,21 @@
+package GraphColoring;
+
+import Exceptions.NotImplementedException;
+
+/**
+ * Abstract class that all constraint solving classes will extent to use the
+ * `SatisfiedConstraint()` method to check if the problem is satisfied
+ * @version 09/08/16
+ */
+public abstract class ConstraintSolver 
+{
+    /**
+     * Determine if the state of a graph satisfied the constraint
+     * @param graph : The graph to check satisfiability on 
+     * @return 
+     */
+    public boolean SatisfiedConstraint(Graph graph)
+    {
+        throw new NotImplementedException();
+    }
+}
