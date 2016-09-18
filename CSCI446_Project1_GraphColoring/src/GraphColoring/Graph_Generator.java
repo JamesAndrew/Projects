1b2d9822225274e22f6bb99417da96c8060842d4
Tyler Wright <twright257@gmail.com>
2016-09-18 13:16:59 -0600

    graph generation wroking with original graph type

--- CSCI446_Project1_GraphColoring/src/GraphColoring/Graph_Generator.java
+++ CSCI446_Project1_GraphColoring/src/GraphColoring/Graph_Generator.java
@@ -72,67 +72,84 @@ public class Graph_Generator
         return (float) Math.sqrt((Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2)));
     }
 
+    /**
+     * Pick node, find closest node to it and connect edge if valid.
+     *
+     * @param graph
+     */
     private void findClosestPoint(Graph graph)
     {
-        Map<Integer, ArrayList> vertices = initializeVertexMap(); 
+        Map<Integer, ArrayList> vertices = initializeVertexMap();
 
         while (!vertices.isEmpty())//!vertices.isEmpty()
         {
             // variable for random index used to select random point
             ArrayList keysAsArray = new ArrayList(vertices.keySet());
             int chosenPt = (int) keysAsArray.get(rand.nextInt(keysAsArray.size()));
+            boolean edgeSet = false;
             System.out.println("Point " + graph.getXPoint(chosenPt) + ", " + graph.getYPoint(chosenPt) + " chosen.");
 
-            int closest = -1;        // closest distance to selected point 
-            float dist;             // current distance being checked
-            float pclosest = 100;   // closest distance to selected point seen thus far
-
-            System.out.println(chosenPt);
-
-            // loop through all points
-            ArrayList<Integer> current = vertices.get(chosenPt);
-            for (Iterator<Integer> iterator = current.iterator(); iterator.hasNext();)//for (int secondPt = 0; secondPt < numVertices; secondPt++)
+            while (!edgeSet && vertices.containsKey(chosenPt))
             {
-                int secondPt = iterator.next();
-                // only consider pairs that do not have an edge between them, both directions
-                if (graph.getEdge(chosenPt, secondPt) != 1 && graph.getEdge(secondPt, chosenPt) != 1)
-                {
-                    dist = distance(graph.getXPoint(chosenPt), graph.getYPoint(chosenPt), graph.getXPoint(secondPt), graph.getYPoint(secondPt));
+                int closest = -1;        // closest distance to selected point 
+                float dist;             // current distance being checked
+                float pclosest = 100;   // closest distance to selected point seen thus far
 
-                    if (dist > 0 && dist < pclosest)
+                System.out.println(chosenPt);
+
+                // loop through all points
+                ArrayList<Integer> current = vertices.get(chosenPt);
+                for (Iterator<Integer> iterator = current.iterator(); iterator.hasNext();)//for (int secondPt = 0; secondPt < numVertices; secondPt++)
+                {
+                    int secondPt = iterator.next();
+                    // only consider pairs that do not have an edge between them, both directions
+                    if (graph.getEdge(chosenPt, secondPt) != 1 && graph.getEdge(secondPt, chosenPt) != 1)
                     {
-                        closest = secondPt;
-                        pclosest = dist;
+                        dist = distance(graph.getXPoint(chosenPt), graph.getYPoint(chosenPt), graph.getXPoint(secondPt), graph.getYPoint(secondPt));
+
+                        if (dist > 0 && dist < pclosest)
+                        {
+                            closest = secondPt;
+                            pclosest = dist;
+                        }
+
+                        System.out.format("Distance between point %d and %d is %f.%n", chosenPt, secondPt, dist);
+                    } else
+                    {
+                        iterator.remove();
                     }
+                }
+                System.out.format("Closest point is %d.%n%n", closest);
 
-                    System.out.format("Distance between point %d and %d is %f.%n", chosenPt, secondPt, dist);
+                if (!current.isEmpty())
+                {
+                    if (!edgesIntersect(graph, chosenPt, closest))
+                    {
+                        graph.setEdge(chosenPt, closest, 1); // add the edge to the graph 
+                        graph.setEdge(closest, chosenPt, 1); // add symmetric edge since graph is symmetric 
+                        edgeSet = true; 
+                    } else
+                    {
+                        System.out.println("Collision Detected\n");
+                        current.remove(current.indexOf(closest));
+                    }
                 } else
                 {
-                    iterator.remove();
-                }
-            }
-            System.out.format("Closest point is %d.%n%n", closest);
-
-            if (!current.isEmpty())
-            {
-                if (!edgesIntersect(graph, chosenPt, closest))
-                {
-                    graph.setEdge(chosenPt, closest, 1); // add the edge to the graph 
-                    graph.setEdge(closest, chosenPt, 1); // add symmetric edge since graph is symmetric 
-                } else
-                {
-                    System.out.println("Collision Detected\n");
-                    current.remove(current.indexOf(closest));
-                }
-            } else
-            {
-                {
-                    vertices.remove(chosenPt);
+                    {
+                        vertices.remove(chosenPt);
+                    }
                 }
             }
         }
     }
 
+    /**
+     * Set map to keep track of valid nodes to make connection to. key = point
+     * number, value = arraylist of currently valid nodes key node can connect
+     * to.
+     *
+     * @return
+     */
     private Map<Integer, ArrayList> initializeVertexMap()
     {
         ArrayList<Integer> possibleConnections = new ArrayList<Integer>();
@@ -173,39 +190,49 @@ public class Graph_Generator
      */
     private boolean edgesIntersect(Graph graph, int point1, int point2)
     {
-        float x1 = graph.getXPoint(point1);
-        float x2 = graph.getXPoint(point2);
-        float y1 = graph.getYPoint(point1);
-        float y2 = graph.getYPoint(point1);
-        float a1 = y2 - y1;
-        float b1 = x1 - x2;
-        float c1 = (a1 * x1) + (b1 * y1);
+        float[] eq1 = getLineEquation(graph, point1, point2);
 
         //find all existing edges and calculate whether or not there is a possible intersection
         for (int i = 0; i < graph.getPoints().length; i++)
         {
             for (int j = 0; j < graph.getPoints().length; j++)
             {
+                //if edge doesn't already exist
                 if (graph.getEdge(i, j) != 0 && !(i == point1 && j == point2))
                 {
-                    float a2 = graph.getYPoint(j) - graph.getYPoint(i);
-                    float b2 = graph.getXPoint(i) - graph.getXPoint(j);
-                    float c2 = (a2 * graph.getXPoint(i)) + (b2 * graph.getYPoint(i));
-
-                    float det = a1 * b2 - a2 * b1;
-                    //parallel lines 
-                    if (det == 0)
+                    float x = 0;
+                    float y = 0;
+                    float[] eq2 = getLineEquation(graph, i, j);
+                    //line 1 is vertical 
+                    if (eq1 == null && eq2 != null)
                     {
-                        return false;
-                    } else
+                        x = graph.getXPoint(point1);
+                        y = (eq2[0] * x) + eq2[1];
+                    } //line 2 is vertical
+                    else if (eq2 == null && eq1 != null)
                     {
-                        float x = (b2 * c1 - b1 * c2) / det;
-                        float y = (a1 * c2 - a2 * c1) / det;
-                        if (checkBounds(graph, x, y, point1, point2) && checkBounds(graph, x, y, i, j))
+                        x = graph.getXPoint(i);
+                        y = (eq1[0] * x) + eq1[1];
+                    } else if (eq1 == null && eq2 == null)
+                    {
+                        //two vertical lines
+                    } //find intersect point between two non vertical lines
+                    else
+                    {
+                        float m = eq2[0] - eq1[0];
+                        float b = eq1[1] - eq2[1];
+                        if (m == 0)
                         {
-                            return true;
+                            return false;
                         }
+                        x = b / m;
+                        y = (eq1[0] * x) + eq1[1];
                     }
+                    if (checkBounds(graph, x, y, point1, point2) && checkBounds(graph, x, y, i, j))
+                    {
+                        return true;
+                    }
+
                 }
             }
         }
@@ -225,19 +252,46 @@ public class Graph_Generator
      */
     private boolean checkBounds(Graph graph, float xIntercept, float yIntercept, int point1, int point2)
     {
-        System.out.format("(%f, %f) 1:%d 2:%d.%n", xIntercept, yIntercept, point1, point2);
+        //System.out.format("(%f, %f) 1:%d 2:%d.%n", xIntercept, yIntercept, point1, point2);
         //if intercept falls within x range of edge
-        if ((xIntercept > graph.getXPoint(point1) && xIntercept < graph.getXPoint(point2))
-                || (xIntercept < graph.getXPoint(point1) && xIntercept > graph.getXPoint(point2)))//(graph.getXPoint(point1) < graph.getXPoint(point2))
+        if ((xIntercept > graph.getXPoint(point1) + .001 && xIntercept < graph.getXPoint(point2) - .001)
+                || (xIntercept < graph.getXPoint(point1) - .001 && xIntercept > graph.getXPoint(point2) + .001))//(graph.getXPoint(point1) < graph.getXPoint(point2))
         {
             //if intercept falls within y range of edge
-            if ((yIntercept > graph.getYPoint(point1) && yIntercept < graph.getYPoint(point2))
-                    || (yIntercept < graph.getYPoint(point1) && yIntercept > graph.getYPoint(point2)))
+            if ((yIntercept > graph.getYPoint(point1) + .001 && yIntercept < graph.getYPoint(point2) - .001)
+                    || (yIntercept < graph.getYPoint(point1) - .001 && yIntercept > graph.getYPoint(point2) + .001))
             {
                 return true;
             }
         }
         return false;
+    }
+
+    /**
+     * get equation of line given two points
+     *
+     * @param graph
+     * @param point1
+     * @param point2
+     * @return array containing slope and y intercept or null if vertical
+     */
+    private float[] getLineEquation(Graph graph, int point1, int point2)
+    {
+        float[] mb = new float[2];
+        float x1 = graph.getXPoint(point1);
+        float y1 = graph.getYPoint(point1);
+        float x2 = graph.getXPoint(point2);
+        float y2 = graph.getYPoint(point2);
+
+        if (x1 != x2)
+        {
+            float m = (y2 - y1) / (x2 - x1);
+            float b = (m * -x1) + y1;
+            mb[0] = m;
+            mb[1] = b;
+            return mb;
+        }
+        return null;
     }
 
     private void printPointLocations(Graph graph)
