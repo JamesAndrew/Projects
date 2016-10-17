6a19a91b68e0ba2fffc3145baa034824066c1a04
Tyler Wright <twright257@gmail.com>
2016-10-16 21:09:28 -0600

    add functionality for continuing in direction already headed

--- CSCI446_Project2_WumpusWorld/src/WumpusWorld/KnowledgeBasedAgent.java
+++ CSCI446_Project2_WumpusWorld/src/WumpusWorld/KnowledgeBasedAgent.java
@@ -5,28 +5,31 @@ import java.util.ArrayList;
 /**
  *
  */
-public class KnowledgeBasedAgent 
+public class KnowledgeBasedAgent
 {
-    private World world; 
-    
-    public KnowledgeBasedAgent() { }
-    
-    public ArrayList<Room> getPath(Room start, Room finish) 
+
+    private World world;
+
+    public KnowledgeBasedAgent()
     {
-        ArrayList<Room> path = new ArrayList<Room>(); 
-        return getPath(start, finish, path); 
     }
-    
-    private ArrayList<Room> getPath(Room current, Room finish, ArrayList<Room> path) 
+
+    public ArrayList<Room> getPath(Room start, Room finish)
     {
-        path.add(current); 
+        ArrayList<Room> path = new ArrayList<Room>();
+        return getPath(start, finish, path);
+    }
+
+    private ArrayList<Room> getPath(Room current, Room finish, ArrayList<Room> path)
+    {
+        path.add(current);
         if (current == finish)
         {
             //path found
-            return path; 
+            return path;
         }
         //agent not already traveling in a particular direction 
-        if (path.size() == 1) 
+        if (path.size() == 1)
         {
             for (int i = current.getRoomColumn() - 1; i <= current.getRoomColumn() + 1; i++)
             {
@@ -38,20 +41,76 @@ public class KnowledgeBasedAgent
                         Room nextRoom = world.getRoom(i, j);
                         if (nextRoom.isSafe())
                         {
-                            ArrayList<Room> possiblePath = getPath(nextRoom, finish, path); 
-                            if (possiblePath != null) {
-                                return possiblePath; 
+                            ArrayList<Room> possiblePath = getPath(nextRoom, finish, path);
+                            if (possiblePath != null)
+                            {
+                                return possiblePath;
                             }
-                        }                         
+                        }
                     }
                 }
             }
-        }
-        else
+        } else
         {
             //try to continue in the direction that the agent was already traveling in
+            int previousX = path.get(-1).getRoomColumn();
+            int previousY = path.get(-1).getRoomRow();
+            int currentX = current.getRoomColumn();
+            int currentY = current.getRoomRow();
+            ArrayList<Room> possiblePath = null;
+
+            if (previousX == currentX)
+            {
+                Room nextRoom;
+                if (previousY < currentY)
+                {
+                    if (currentY + 1 <= world.getSize())
+                    {
+                        nextRoom = world.getRoom(currentX, currentY + 1);
+                        if (nextRoom.isSafe())
+                        {
+                            possiblePath = getPath(nextRoom, finish, path);
+                        }
+                    }
+                } else if (currentY - 1 >= 0)
+                {                    
+                    {
+                        nextRoom = world.getRoom(currentX, currentY - 1);
+                        if (nextRoom.isSafe())
+                        {
+                            possiblePath = getPath(nextRoom, finish, path);
+                        }
+                    }
+                }
+            } else if (previousY == currentY)
+            {
+                Room nextRoom;
+                if (previousX < currentX)
+                {
+                    if (currentX + 1 <= world.getSize())
+                    {
+                        nextRoom = world.getRoom(currentX + 1, currentY);
+                        if (nextRoom.isSafe())
+                        {
+                            possiblePath = getPath(nextRoom, finish, path);
+                        }
+                    }
+                } else if (currentX - 1 >= 0)
+                {
+                    nextRoom = world.getRoom(currentX - 1, currentY);
+                    if (nextRoom.isSafe())
+                    {
+                        possiblePath = getPath(nextRoom, finish, path);
+                    }
+                }
+            }
+            if (possiblePath != null)
+            {
+                return possiblePath;
+            }
         }
         //path not found from this room 
-        return null; 
+        return null;
     }
+
 }
