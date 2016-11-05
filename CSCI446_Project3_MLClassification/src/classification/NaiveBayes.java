f5d14e3a4a808a7e5e77039f0eaa87d6efaacbce
JamesAndrew <jandrewshields@gmail.com>
2016-11-05 14:44:54 -0600

    added NB class and main class

--- CSCI446_Project3_MLClassification/src/classification/NaiveBayes.java
+++ CSCI446_Project3_MLClassification/src/classification/NaiveBayes.java
@@ -13,6 +13,15 @@ public class NaiveBayes
 {
     public NaiveBayes()
     {
+        System.out.println("=== Naive Bayes Experiment ===");
+        DummyNB();
+    }
+    
+    private void DummyNB()
+    {
+        System.out.println("=== Dummy Naive Bayes Method ===");
+        int [][] data = { {1000025, 5, 1, 1, 1, 2, 1, 3, 1, 1, 2},
+            {1002945, 5, 4, 4, 5, 7, 10, 3, 2, 1, 2} };
         
     }
 }
