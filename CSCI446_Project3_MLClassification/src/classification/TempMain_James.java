8477a2728b638020f8bcfe4e41590b9b25456899
JamesAndrew <jandrewshields@gmail.com>
2016-11-06 17:20:31 -0700

    started gathering statistics for small subset of breast cancer data.

---
+++ CSCI446_Project3_MLClassification/src/classification/TempMain_James.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classification;
+
+import java.io.FileNotFoundException;
+
+/**
+ *
+ * @author James
+ */
+public class TempMain_James 
+{
+    public static void main(String[] args) throws FileNotFoundException 
+    {
+        NaiveBayes nb = new NaiveBayes();
+    }
+}
