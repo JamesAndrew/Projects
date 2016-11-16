ac5c13e36daccdb6970ef5ea31afaec6e5647e1b
David Rice <davidrice363@gmail.com>
2016-11-16 15:40:21 -0700

    Distance function for categorical features finished

--- CSCI446_Project3_MLClassification/src/classification/NearestNeighbor.java
+++ CSCI446_Project3_MLClassification/src/classification/NearestNeighbor.java
@@ -1,5 +1,8 @@
 package classification;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class NearestNeighbor extends Categorizer
 {
     // tunable parameters
@@ -39,9 +42,57 @@ public class NearestNeighbor extends Categorizer
         // for each point in the testing fold...
         for (int i = 0; i < testingFold.getVectors().length; i++)
         {
+            // compute the distance from that point to all opints in the trainingSet
+            Vector currentPoint = testingFold.getVectors()[i];
+            ArrayList<DistanceAndIndex> DIValues = new ArrayList<>();
+            for (int j = 0; j < trainingSet.getVectors().length; j++)
+            {
+                Vector otherPoint = trainingSet.getVectors()[j];
+                DistanceAndIndex currentDist = calculateDistance(currentPoint, otherPoint, j);
+            }
             
         }
         throw new UnsupportedOperationException();
     }
     
+    /**
+     * Calculate categorical (discrete) distance between two vectors. Also
+     * track which index this is linked to
+     * 
+     * The equation used is discussed in 
+     * http://ce.sharif.edu/courses/84-85/2/ce324/resources/root/Supplementary%20Materials%20for%20Final%20Exam/Data%20Mining%20(Classification)pdf.pdf
+     * on page 11
+     * 
+     * @param a : sample vector
+     * @param b : training vector
+     * @param index : the index of the training vector in the training set array
+     * @return : A data object that contains the distance and index
+     */
+    private DistanceAndIndex calculateDistance(Vector a, Vector b, int index)
+    {
+        if (a.getValue().length != b.getValue().length)
+            throw new RuntimeException("vectors a and b are of different lengths");
+        
+        double sum = 0.0;
+        for (int i = 0; i < a.getValue().length; i++)
+        {
+            if (a.getValue()[i] == b.getValue()[i]) sum += 1.0;
+        }
+        double distance = Math.sqrt(sum);
+        
+        DistanceAndIndex DIRetruned = new DistanceAndIndex(distance, index);
+        return DIRetruned;
+    }
+    
+    class DistanceAndIndex
+    {
+        double distance;
+        int    index;
+        
+        public DistanceAndIndex(double dist, int indx)
+        {
+            distance = dist;
+            index = indx;
+        }
+    }
 }
