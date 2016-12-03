b97c1405914ccece88816a9c4032fb304ca79a86
JamesAndrew <jandrewshields@gmail.com>
2016-12-03 14:34:31 -0700

    Added Racetrack package

--- CSCI446_Project4_Racetrack/src/racetrack/Driver.java
+++ CSCI446_Project4_Racetrack/src/racetrack/Driver.java
@@ -3,18 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package racetrack;
 
 /**
  *
  * @author James
  */
 public class Driver {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
     
 }
