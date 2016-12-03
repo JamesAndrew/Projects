1c01b07cd5fcfbbc6834c31b029af11b1cd8d2fd
JamesAndrew <jandrewshields@gmail.com>
2016-12-03 16:30:34 -0700

    started loading tracks

--- CSCI446_Project4_Racetrack/src/racetrack/Racetrack.java
+++ CSCI446_Project4_Racetrack/src/racetrack/Racetrack.java
@@ -11,5 +11,40 @@ package racetrack;
  */
 public class Racetrack 
 {
+    private char [][] track;
     
+    public Racetrack(int r, int c)
+    {
+        track = new char[r][c];
+    }
+    
+    public void setTrack(int r, int c, char t)
+    {
+        track[r][c] = t;
+    }
+    
+    public void printTrack()
+    {
+        int rowLength = track.length;
+        int colLength = track[0].length;
+        
+        for (int r = 0; r < rowLength; r++)
+        {
+            for (int c = 0; c < colLength; c++)
+            {
+                System.out.print(track[r][c]);
+            }
+            System.out.println();
+        }
+    }
+    
+    public void getCrashLocations()
+    {
+        
+    }
+    
+    public void getTrackHeatmap()
+    {
+        
+    }
 }
