5668d2ea64973006ff0a39939c4ada1c2299d0d0
David Rice <davidrice363@gmail.com>
2016-12-10 19:25:36 -0700

    Added QLearning class

---
+++ CSCI446_Project4_Racetrack/src/racetrack/QLearning.java
@@ -0,0 +1,24 @@
+
+package racetrack;
+
+public class QLearning 
+{
+    // tunable parameters //
+    // threshold to stop training the race track
+    private final double epsilon = 0.00001;    
+    // discount factor - low values decrement additive rewards
+    private final double gamma = 0.95;
+    // learning factor - lower values take longer to converge but give better results
+    private final double alpha = 0.5;
+    // track currently being worked with 
+    private final Racetrack track;
+    
+    /**
+     * Constructor
+     * @param in_track : race track to assign [state, action] values to
+     */
+    public QLearning(Racetrack in_track)
+    {
+        track = in_track;
+    }
+}
