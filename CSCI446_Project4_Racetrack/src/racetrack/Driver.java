26f9727ff0b5b67be74858ab2c5ce8cbacb0cebb
David Rice <davidrice363@gmail.com>
2016-12-12 16:39:38 -0700

    Updated main driver class to run correctly

--- CSCI446_Project4_Racetrack/src/racetrack/Driver.java
+++ CSCI446_Project4_Racetrack/src/racetrack/Driver.java
@@ -1,27 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package racetrack;
 
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
  */
 public class Driver 
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException
     {
-        try
-        {
-            Experiment e = new Experiment();
-        } catch (IOException ex)
-        {
-            Logger.getLogger(Driver.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        Experiment experiment = new Experiment();
+        experiment.runValueIteration();
+//        experiment.runQLearning();
     }
 }
